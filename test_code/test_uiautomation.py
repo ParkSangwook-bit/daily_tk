e71bd54fcf7e6eee2cb682745f7692e4de80bf41
from_yk <qkrtkddnr0118@gmail.com>
2024-11-19 20:36:31 +0900

    testing kakao uiautomation
    -

--- test_code/test_uiautomation.py
+++ test_code/test_uiautomation.py
@@ -1,15 +1,32 @@
 import uiautomation as auto
 import subprocess
+import psutil
 
-# 카카오톡 프로그램 실행 파일 경로
-kakao_path = r"C:\Program Files (x86)\Kakao\KakaoTalk\KakaoTalk.exe"
+# 카카오 실행 여부 검사 및 실행
+def ensure_kakao_running():
+    if is_kakao_running():
+        pass
+    else:
+        launch_kakao()
 
-# Popen을 이용해 카카오톡 실행
-subprocess.Popen([kakao_path], shell=False)
+# 카카오톡 프로세스 감지
+def is_kakao_running():
+    for proc in psutil.process_iter(attrs=['pid', 'name']):
+        if proc.info['name'].lower() == 'kakaotalk.exe':
+            print("\'KakaoTalk.exe\' ")
+            return True
+        else:
+            print("Failed detecting \'KakaoTalk.exe\' process")
+            return False
 
-kakao_window = auto.WindowControl(searchDepth=1, Name="카카오톡")
+# 카카오톡 프로세스 실행
+def launch_kakao():
+    # 카카오톡 프로그램 실행 파일 경로
+    kakao_path = r"C:\Program Files (x86)\Kakao\KakaoTalk\KakaoTalk.exe"
 
-friend_list = kakao_window.MenuItemControl(Name="친구")
-if friend_list.Exists:
-    print("친구 메뉴를 찾았습니다.")
-    friend_list.Click()
+    # Popen을 이용해 카카오톡 실행
+    subprocess.Popen([kakao_path], shell=False)
+
+
+if __name__ == "__main__":
+    ensure_kakao_running()
